8b1de6c48b18427279ac6966c482d1140295bcfb
chloeblazey <chloeblazey@gmail.com>
2023-11-07 22:59:20 -0800

    map of all possible game outcomes finished. playRound function initialized.

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ function getComputerChoice() {
   let choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * 3)];
 }
+
+// Maps [player, computer] choices to game outcomes.
+const POSSIBLE_OUTCOMES = new Map();
+
+POSSIBLE_OUTCOMES.set(["rock", "rock"], "It's a tie :/");
+POSSIBLE_OUTCOMES.set(["rock", "paper"], "Computer Victory! All hail the robot overlords!");
+POSSIBLE_OUTCOMES.set(["rock", "scissors"], "Player Wins! The indomitable human spirit lives on!");
+POSSIBLE_OUTCOMES.set(["paper", "rock"], "Player Wins! The indomitable human spirit lives on!");
+POSSIBLE_OUTCOMES.set(["paper", "paper"], "It's a tie :/");
+POSSIBLE_OUTCOMES.set(["paper", "scissors"], "Computer Victory! All hail the robot overlords!");
+POSSIBLE_OUTCOMES.set(["scissors", "rock"], "Computer Victory! All hail the robot overlords!");
+POSSIBLE_OUTCOMES.set(["scissors", "paper"], "Player Wins! The indomitable human spirit lives on!");
+POSSIBLE_OUTCOMES.set(["scissors", "scissors"], "It's a tie :/");
+
+// Plays a single round of RPS.
+function playRound(playerSelection, ComputerSelection) {
+  return true;
+}
