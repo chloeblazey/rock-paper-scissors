7545cbbf5b3a6c9d3611e4235089506bcd89f81a
chloeblazey <chloeblazey@gmail.com>
2023-11-07 23:50:56 -0800

    adjusts POSSIBLE_OUTCOMES keys from array object to string

--- index.js
+++ index.js
@@ -4,20 +4,24 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)];
 }
 
-// Maps [player, computer] choices to game outcomes.
+// Maps "player computer" choices to game outcomes.
 const POSSIBLE_OUTCOMES = new Map();
 
-POSSIBLE_OUTCOMES.set(["rock", "rock"], "It's a tie :/");
-POSSIBLE_OUTCOMES.set(["rock", "paper"], "Computer Victory! All hail the robot overlords!");
-POSSIBLE_OUTCOMES.set(["rock", "scissors"], "Player Wins! The indomitable human spirit lives on!");
-POSSIBLE_OUTCOMES.set(["paper", "rock"], "Player Wins! The indomitable human spirit lives on!");
-POSSIBLE_OUTCOMES.set(["paper", "paper"], "It's a tie :/");
-POSSIBLE_OUTCOMES.set(["paper", "scissors"], "Computer Victory! All hail the robot overlords!");
-POSSIBLE_OUTCOMES.set(["scissors", "rock"], "Computer Victory! All hail the robot overlords!");
-POSSIBLE_OUTCOMES.set(["scissors", "paper"], "Player Wins! The indomitable human spirit lives on!");
-POSSIBLE_OUTCOMES.set(["scissors", "scissors"], "It's a tie :/");
+POSSIBLE_OUTCOMES.set("rock rock", "It's a tie :/");
+POSSIBLE_OUTCOMES.set("rock paper", "Computer Victory! All hail the robot overlords!");
+POSSIBLE_OUTCOMES.set("rock scissors", "Player Wins! The indomitable human spirit lives on!");
+POSSIBLE_OUTCOMES.set("paper rock", "Player Wins! The indomitable human spirit lives on!");
+POSSIBLE_OUTCOMES.set("paper paper", "It's a tie :/");
+POSSIBLE_OUTCOMES.set("paper scissors", "Computer Victory! All hail the robot overlords!");
+POSSIBLE_OUTCOMES.set("scissors rock", "Computer Victory! All hail the robot overlords!");
+POSSIBLE_OUTCOMES.set(["scissors paper"], "Player Wins! The indomitable human spirit lives on!");
+POSSIBLE_OUTCOMES.set(["scissors scissors"], "It's a tie :/");
 
 // Plays a single round of RPS.
-function playRound(playerSelection, ComputerSelection) {
-  return true;
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toLowerCase().trim();
+  computerSelection = computerSelection.toLowerCase().trim();
+  return POSSIBLE_OUTCOMES.get([playerSelection, computerSelection]);
 }
+console.log(POSSIBLE_OUTCOMES.has(["rock", "rock"]));
+console.log(playRound("rock", "paper"));
