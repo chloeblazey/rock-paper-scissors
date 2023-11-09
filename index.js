9329f4e95f0ee694629813ec8584e5f505e35f36
chloeblazey <chloeblazey@gmail.com>
2023-11-08 22:39:20 -0800

    Add user choice buttons
    Adds a rock, paper and scissors button to the GUI. When clicked, these
    buttons will trigger a single round of Rock Paper Scissors. Round
    results are not displayed to the user.
    Note that the entire game no longer triggers on page load, but instead
    plays a single round on button click.

--- index.js
+++ index.js
@@ -24,36 +24,51 @@ function playRound(playerSelection, computerSelection) {
   return POSSIBLE_OUTCOMES.get(playerSelection + " " + computerSelection);
 }
 
+// Adds event listeners to the player buttons that trigger a round of rps
+const buttons = document.querySelectorAll("button");
+buttons.forEach((el) => {
+    el.addEventListener("click", () => {
+      playRound(el.textContent, getComputerChoice());
+    })
+  }
+);
+
 // Plays a game of Rock Paper Scissor, 5 rounds.
 function game() {
   alert("You are now playing rock paper scissors.");
 
-  const NUM_ROUNDS = 5;
   let playerScore = 0;
   let computerScore = 0;
   let playerSelection;
   let computerSelection;
   let outcome;
 
-  for (let i = 0; i < NUM_ROUNDS; i++) {
-    alert("Let's play round " + (i+1) + ".");
+  alert("Let's play a round.");
 
-    // Gathers the player's input then calculates the outcome
-    playerSelection = prompt("Type your choice: rock, paper, or scissors?");
-    computerSelection = getComputerChoice();
-    outcome = playRound(playerSelection, computerSelection);
+  // Gathers the player's input then calculates the outcome
+  playerSelection = prompt("Type your choice: rock, paper, or scissors?");
+  computerSelection = getComputerChoice();
+  outcome = playRound(playerSelection, computerSelection);
 
-    // Updates the score
-    if (outcome === "Player Wins! The indomitable human spirit lives on!") {
-      ++playerScore;
-    } else if (outcome === "Computer Victory! All hail the robot overlords!") {
-      ++computerScore;
-    }
-
-    alert("You chose: " + playerSelection + "\n" + "The computer chose: " + computerSelection);
-    alert(outcome + "\n" + "Player: " + playerScore + "    Computer: " + computerScore);
+  // Updates the score
+  if (outcome === "Player Wins! The indomitable human spirit lives on!") {
+    ++playerScore;
+  } else if (outcome === "Computer Victory! All hail the robot overlords!") {
+    ++computerScore;
   }
-}
 
-// Plays game
-game()
+  // Announces the score
+  alert("You chose: " + playerSelection + "\nThe computer chose: " + computerSelection);
+  alert(outcome + "\nPlayer: " + playerScore + "    Computer: " + computerScore);
+  
+  /* 
+  // Calculates game winner and announces final score
+  if (playerScore < computerScore) {
+    alert("Computer has won... you lose.. but you are always a winner in my heart :-)\n\nFinal Score:\nPlayer: " +  playerScore + "    Computer: " + computerScore);
+  } else if (playerScore == computerScore) {
+    alert("wowee an honest tie... no body wins.. but no body loses?\n\nFinal Score:\nPlayer: " +  playerScore + "    Computer: " + computerScore);
+  } else if (playerScore > computerScore) {
+    alert("congraulation ! you are a winner ! lets all be friends and celebrate ^_^\n\nFinal Score:\nPlayer: " +  playerScore + "    Computer: " + computerScore);
+  }
+  */
+}
